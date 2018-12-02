3cc7fea883a459f0935c67807b98f7bf5bd16ee9
Michał Ciernia <ziemniak1996@gmail.com>
2018-12-02 13:04:14 +0100

    validation added

--- Kolos_1_12/Kolo.cs
+++ Kolos_1_12/Kolo.cs
@@ -19,11 +19,15 @@ namespace Kolos_1_12
             }
             set
             {
+                /* to nie tutaj - setter nie powinien wchodzic w petle - przynajmniej nie w takim przypadku (kiedy jest on tylko dostepem do zmiennej)
                 while (string.IsNullOrEmpty(value))
                 {
                     Console.WriteLine("Nazwa nie moze być pusta!");
                     value = Console.ReadLine();
                 }
+                */
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Błędna wartość! wartość nie może być pusta");
 
                 s_nazwa = value;
             }
@@ -37,21 +41,58 @@ namespace Kolos_1_12
             }
             set
             {
+                /* to nie tutaj - setter nie powinien wchodzic w petle - przynajmniej nie w takim przypadku (kiedy jest on tylko dostepem do zmiennej)
                 while (value < 1 )
                 {
                     Console.WriteLine("Promien nie moze byc mniejszy od 1!");
-                    value = float.Parse(Console.ReadLine());
+                    value = float.Parse(Console.ReadLine()); // <--- tu program się wysypie w momencie gdy wartość nie będzie liczbą
                 }
+                */
+                if (value < 1)
+                    throw new Exception("Błędna wartość! promien nie moze byc < 1");
                 f_promien = value;
             }
         }
 
+
         public override void WprowadzDane()
         {
-            Console.WriteLine("Podaj dane: \n Podaj nazwe: ");
-            Nazwa = Console.ReadLine();
+
+            Console.WriteLine("Podaj dane:");
+
+            while (true)
+            {
+                Console.Write("	podaj nazwe: ");
+                try
+                {
+                    Nazwa = Console.ReadLine();
+                    break; // <-- jesli wszystko ok - przerywamy petle
+                }
+                catch (Exception e) // <-- przechwytujemy wyjatek z settera - wyswietlamy informacje - zerujemy
+                {
+                    Console.WriteLine("Błąd! {0}", e.Message);
+                }
+
+            }
+
+            while (true)
+            {
+                Console.Write("	podaj promien: ");
+                try
+                {
+                    Promien = float.Parse(Console.ReadLine());
+                    break; // <-- jesli wszystko ok - przerywamy petle
+                }
+                catch (Exception e) // <-- przechwytujemy wyjatek z settera - wyswietlamy informacje - zerujemy
+                                    // <-- przy okazji wyjatek "zlapie nam" blednie wprowadzona wartosc liczbowa (np. tekst)
+                {
+                    Console.WriteLine("Błąd! {0}", e.Message);
+                }
+
+            }
+
             Console.WriteLine("\nPodaj promien");
-            Promien = float.Parse(Console.ReadLine());
+
         }
         public override string WyswietlInformacje()
         {
